9599281d36cb126b15c40ff3dcc6ca0515c0cb37
iossos-proton <iossos@proton.me>
2024-08-23 12:12:01 +0530

    fixed download name check

--- rssbox/seedr_client.py
+++ rssbox/seedr_client.py
@@ -284,7 +284,7 @@ class SeedrClient(Heartbeat):
 
         for seedr in seedrs:
             download = seedr.download
-            if not download or not download.download_name:
+            if not download:
                 logger.info(
                     f"Seedr downloaded but no download found for {seedr.download_id} ({seedr.id})"
                 )
